b6cd1fee8a548d1d91b273307b1dadf462293f73
TiffMonique <tiffanymatamoros72@gmail.com>
2021-12-22 00:21:24 -0600

    Cambio de carpetas

---
+++ Cue 2/Cue 2/RE_CUE_2_ ENVIANDO DATOS DESDE UN FORMULARIO/src/main/java/com/edutecno/MetodoGet.java
@@ -0,0 +1,48 @@
+package com.edutecno;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class MetodoGet
+ */
+public class MetodoGet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MetodoGet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		
+		PrintWriter printWriter = response.getWriter();
+		
+		String usuario = request.getParameter("usuario");
+		
+		
+		printWriter.println("El nombre enviado es: "+ usuario);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
